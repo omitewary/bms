d80835697957e3e67f22b9687c92475d70f43f95
omitewary <omitewary@gmal.com>
2024-12-03 08:33:13 +0530

    adding error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,28 @@
-import express from 'express';
-import {json } from 'body-parser';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
 
-import { currentUserRouter } from './routes/current-user';
-import { signupRouter } from './routes/signup';
+import { currentUserRouter } from "./routes/current-user";
+import { signupRouter } from "./routes/signup";
+import { signinrouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
 
-app.use(currentUserRouter)
-app.use(signupRouter)
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinrouter);
+app.use(signoutRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000!")
-})
+  console.log(`Application restarted at ${new Date().toISOString()}`);
+});
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
 const router = express.Router();
 
-router.get('/api/users/signup', (req, res) => {
-    res.send("Hi there sign up!")
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
 
-export {router as signupRouter}
+    const { email, password } = req.body;
+    console.log("user created!!!");
+    throw new DatabaseConnectionError();
+
+    res.send({});
+  }
+);
+
+export { router as signupRouter };
